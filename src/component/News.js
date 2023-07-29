2997f08ce61eb3bea64070e6b574a03bcdcbc6bb
Ayushi Priya <ayushipriya@example.com>
2023-07-29 16:10:00 +0530

    initail commit

---
+++ src/component/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    console.log("Hello i am a constructor from news component");
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=5f8dc55477224d5ba7e77d2cff438b3a";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+  render() {
+    return (
+      <div className="container my-3">
+        <h1>NewsMonkey - Top Headlines</h1>
+
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0,100) : ""}
+                  description={element.description ? element.description.slice(0,139) : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
